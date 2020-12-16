fce175622c6763924dbeaa172e25b1af68ba67ef
Anna Heath <31513217+annaheath@users.noreply.github.com>
2020-12-16 09:37:47 -0500

    Initialise

---
+++ 02_data/01_Data_Inputs.R
@@ -0,0 +1,15 @@
+################################################################################
+#### Model Inputs from Data for the Chemotherapy Model
+################################################################################
+
+## Total Number of Patients in Observed Data
+n_patients <- 111 
+
+## Number of Patients with side effects
+n_side_effects <- 27
+
+## Number of Hospitalised Patients
+n_hospitalised <- 17
+
+## Number of Patients who Died
+n_died <- 1
---
+++ 02_data/02_Assumption_Inputs.R
@@ -0,0 +1,55 @@
+################################################################################
+#### Model Inputs from Assumptions for the Chemotherapy Model
+################################################################################
+
+## Time Horizon
+time_horizon <- 20
+
+## Number of Patients in the patient population
+n_population <- 1000
+
+## Parameters for the PSA distribution of the risk reduction of side effects
+rr_side_effects_mu <- 0.81
+rr_side_effects_sd <- 0.2
+
+## Parameters for the PSA distribution of the recovery time for patients who are
+## not hospitalised
+t_recovery_home_mu <- 0.35
+t_recovery_home_sd <- sqrt(0.058)
+
+## Parameters for the PSA distribution of the recovery time for patients who are
+## hospitalised
+t_recovery_hosp_mu <- 0.28
+t_recovery_hosp_sd <- sqrt(0.052)
+
+## Parameters for the PSA distribution of the costs of treating patients at home
+c_home_care_mu <- 830
+c_home_care_sd <- sqrt(150)
+
+## Parameters for the PSA distribution of the costs of treating patients in
+## hospital
+c_hospital_mu <- 2400
+c_hospital_sd <- sqrt(1880)
+
+## Parameters for the PSA distribution of the one-off cost of death
+c_death_mu <- 1710
+c_death_sd <- sqrt(760)
+
+
+## Parameters for the PSA distribution of the utility for recovered patients
+u_recovery_mu <- 0.98
+u_recovery_sd <- sqrt(0.001)
+
+## Parameters for the PSA distribution of the utility of patients who are treated
+## at home. 
+u_home_care_mu <- 0.7
+u_home_care_sd <- sqrt(0.02)
+
+## Parameters for the PSA distribution of the utility of treating patients in 
+## hospital
+u_hospital_mu <- 0.3
+u_hospital_sd <- sqrt(0.03)
+
+## Drug costs
+c_treatment_1 <- 120
+c_treatment_2 <- 10300
